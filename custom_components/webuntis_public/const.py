4e6bb243b4724232f7ae31d42aac67c285a137fd
pevau <75302240+pevau@users.noreply.github.com>
2026-09-23 10:48:14 +0200

    Add WebUntis Public integration core

---
+++ custom_components/webuntis_public/const.py
@@ -0,0 +1,38 @@
+DOMAIN = "webuntis_public"
+
+CONF_SERVER = "server"
+CONF_SCHOOL = "school"
+CONF_SCHOOL_NAME = "school_name"
+CONF_CLASS_ID = "class_id"
+CONF_CLASS_NAME = "class_name"
+CONF_CLASS_IDS = "class_ids"
+CONF_CLASS_NAMES = "class_names"
+
+CONF_SETUP_METHOD = "setup_method"
+CONF_SEARCH_QUERY = "search_query"
+CONF_SCHOOL_RESULT = "school_result"
+CONF_PUBLIC_LINK = "public_link"
+
+METHOD_SEARCH = "search"
+METHOD_PUBLIC_LINK = "public_link"
+METHOD_MANUAL = "manual"
+
+# Options
+OPT_SHOW_CANCELLED = "show_cancelled"
+OPT_TITLE_FORMAT = "title_format"
+OPT_SHOW_TEACHER = "show_teacher"
+OPT_SHOW_ROOM = "show_room"
+OPT_SHOW_CLASS = "show_class"
+OPT_NEXT_LESSON_DAYS = "next_lesson_days"
+
+TITLE_SUBJECT = "subject"
+TITLE_SUBJECT_ROOM = "subject_room"
+TITLE_SUBJECT_TEACHER = "subject_teacher"
+TITLE_SUBJECT_ROOM_TEACHER = "subject_room_teacher"
+
+DEFAULT_SHOW_CANCELLED = True
+DEFAULT_TITLE_FORMAT = TITLE_SUBJECT
+DEFAULT_SHOW_TEACHER = True
+DEFAULT_SHOW_ROOM = True
+DEFAULT_SHOW_CLASS = True
+DEFAULT_NEXT_LESSON_DAYS = 14
